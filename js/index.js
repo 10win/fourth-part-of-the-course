02b049914519fdf7ae66029a29417e5e2c2e66f1
Nik <10winfiles@gmail.com>
2022-09-30 18:14:01 +0300

    fourth part of the course: added modal window

--- js/index.js
+++ js/index.js
@@ -100,4 +100,50 @@ document.addEventListener("DOMContentLoaded", () => {
 
     setClock(".timer", deadline);
 
+//    Modal
+
+    const modalTrigger = document.querySelectorAll("[data-modal]"),
+          modal = document.querySelector(".modal"),
+          modalCloseBtn = document.querySelector("[data-close]");
+
+    function openModal() {
+        modal.classList.toggle("show");
+        document.body.style.overflow = "hidden";
+        clearInterval(modalTimerId);
+    }
+
+    function closeModal() {
+        modal.classList.toggle("show");
+        document.body.style.overflow = "";
+    }
+
+    modalTrigger.forEach(btn => {
+        btn.addEventListener("click", openModal);
+    });
+
+    modalCloseBtn.addEventListener("click", closeModal);
+
+    modal.addEventListener("click", (event) => {
+       if (event.target === modal) {
+           closeModal();
+       }
+    });
+
+    document.addEventListener("keydown", (event) => {
+        if (event.code === "Escape" && modal.classList.contains("show")) {
+            closeModal();
+        }
+    });
+
+    const modalTimerId = setTimeout(openModal, 5000);
+
+    function showModalByScroll() {
+        if (window.pageYOffset + document.documentElement.clientHeight >=
+        document.documentElement.scrollHeight - 1) {
+            openModal();
+            window.removeEventListener("scroll", showModalByScroll);
+        }
+    }
+
+    window.addEventListener("scroll", showModalByScroll);
 });
